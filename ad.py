3b1648037f4f2286d5bc7d3685cc8d4f22d0136b
chand796 <chandanakallurchde@gmail.com>
2024-03-05 15:00:17 +0530

    variables program

--- ad.py
+++ ad.py
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 a=20
 b=30
 c=3
 print(a+b)
 print("hello")
 print("add address=")//it prints address.
+=======
+this is add file
+this is second time
+>>>>>>> d9b9568 (second commit in cherry pick)
