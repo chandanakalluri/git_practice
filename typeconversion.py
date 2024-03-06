45c7ad507c5cbc2c0de42f47743fd1b7f0d2c001
chand796 <chandanakallurchde@gmail.com>
2024-03-06 18:45:22 +0530

    type conversion done

--- typeconversion.py
+++ typeconversion.py
@@ -8,7 +8,16 @@ c=66.9
 print(c)
 print(type(c))
 d=int(c)
-print(c)
-print(type(c))
+print(d)
+print(type(d))
+#converting built-in data types
+l=[1,2,3,4]
+l1=set(l)
+print(type(l1))
+l2=tuple(l)
+print(type(l2))
+#l3=dict(l)
+#print(type(l3))#here error came
+
 
 
