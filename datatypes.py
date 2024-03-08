44abddf490f9d2374dd13d24f2c816d29eef1ba1
chand796 <chandanakallurchde@gmail.com>
2024-03-08 15:48:58 +0530

    remove duplicates whith in function

--- datatypes.py
+++ datatypes.py
@@ -4,6 +4,10 @@ c=66.6
 d=True
 print(a)
 print(type(a))
+print(b)
 print(type(b))
+print(c)
 print(type(c))
+print(d)
 print(type(d))
+#Built in datatypes
---
+++ dfunction.py
@@ -0,0 +1,23 @@
+print("lists built in functions:")
+l1=[1,3,45,1,0]
+l1.append(7)#adding element
+print(l1,"adding element")
+l1.remove(7)#removing element
+print(l1,"removing element")
+a=sorted(l1)
+print(a,"sorting elements")#sorting elements
+l1.reverse()
+print(l1, "reverse")
+l1.sort()
+print(l1,"sorting")
+print("4 occurs",l1.count(1),"times")
+print(l1.pop(0),l1,"pop")
+print("functions inside set data type:")
+s = {2, 4, 6, 8, 3, 1}
+print(s)
+print(s.max(),"minimum ele")
+
+
+
+
+
